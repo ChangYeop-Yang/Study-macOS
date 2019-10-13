666833eaebc0f7644b6ae587604b296edc0ca477
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2019-10-13 14:35:29 +0900

    https://dev-dream-world.tistory.com/93
    https://dev-dream-world.tistory.com/93

---
+++ GetSerialNumberMacOS.swift
@@ -0,0 +1,13 @@
+func deviceSerialNumber() -> String? {
+         
+        let platformExpert = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"))
+         
+        guard platformExpert > 0,
+                let serial = (IORegistryEntryCreateCFProperty(platformExpert, kIOPlatformSerialNumberKey as CFString, kCFAllocatorDefault, 0).takeUnretainedValue() as? String)?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) else {
+            return nil
+        }
+         
+        IOObjectRelease(platformExpert)
+         
+        return serial
+ }

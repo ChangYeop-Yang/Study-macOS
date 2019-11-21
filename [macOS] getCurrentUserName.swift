ac8e95399d23cbc8bd80a06fd7746ac005ad4138
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2019-11-21 09:39:16 +0900

    Update [macOS] getCurrentUserName.swift

--- [macOS] getCurrentUserName.swift
+++ [macOS] getCurrentUserName.swift
@@ -1,9 +1,32 @@
+  
+/*
+ * Copyright (c) 2019 양창엽. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
         macOS 환경에서의 로그인 된 실 사용자의 이름을 구하여 반환합니다.
         
         - Returns: String? (Optional)
-     */
-    func getCurrentUserName() -> String? {
+*/
+func getCurrentUserName() -> String? {
         // Creates a new session used to interact with the dynamic store maintained by the System Configuration server.
         let store = SCDynamicStoreCreate(nil, "GetConsoleUser" as CFString, nil, nil)
 
@@ -14,4 +37,4 @@
         }
 
         return result as String?
-    }
+}
